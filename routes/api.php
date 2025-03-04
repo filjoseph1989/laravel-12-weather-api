7b879143b346d8ef2f42dbb84f7d043bca7064bd
Fil <filjoseph22@gmail.com>
2025-03-05 03:01:21 +0800

    used sanctum middle in route

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::middleware('auth:sanctum')->get('/weather', [WeatherController::class, 'getWeather'])->name('current_weather');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
