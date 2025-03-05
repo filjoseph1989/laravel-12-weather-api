6d967e427f0d8631e0063586abc776c49578fdb4
Fil <filjoseph22@gmail.com>
2025-03-05 23:02:05 +0800

    added new routes and endpoint to interact with posts

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,14 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->get('/weather', [WeatherController::class, 'getWeather'])->name('current_weather');
-Route::middleware('auth:sanctum')->get('/posts', [PostController::class, 'allPost'])->name('allPost');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::middleware('auth:sanctum')->get('/weather', [WeatherController::class, 'getWeather'])->name('current_weather');
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::get('/posts', [PostController::class, 'getPaginatedPost'])->name('getPaginatedPost');
+    Route::get('/posts/{post}', [PostController::class, 'getPost'])->name('getPost');
+    Route::post('/posts', [PostController::class, 'store'])->name('createPost');
+    Route::put('/posts/{post}', [PostController::class, 'update'])->name('updatePost');
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('deletePost');
+});
