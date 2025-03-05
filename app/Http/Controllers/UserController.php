4f0d66c2581d04c13a4e658c677975dc8d95af23
Fil <filjoseph22@gmail.com>
2025-03-06 02:51:19 +0800

    further improved getUser method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,26 +4,32 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use App\Traits\ApiResponse;
 use Illuminate\Support\Facades\Cache;
-
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class UserController extends Controller
 {
+    use AuthorizesRequests, ApiResponse;
+
+    /**
+     * Return information about the current user
+     *
+     * @param \App\Models\User $user
+     * @return mixed|\Illuminate\Http\JsonResponse
+     */
     public function getUser(User $user)
     {
         try {
+            $this->authorize('view', $user);
+
+            $user->loadMissing(['posts']);
+
             $cacheKey = "user_{$user->id}";
             $userData = Cache::remember($cacheKey, 900, fn() => new UserResource($user));
-            return response()->json([
-                'success' => true,
-                'message' => 'User retrieved successfully.',
-                'data' => $userData
-            ], 200);
+
+            return $this->successResponse($userData, 'User retrieved successfully.');
         } catch (\Throwable $th) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Failed to retrieve user.',
-                'error' => $th->getMessage()
-            ], 500);
+            return $this->errorResponse('Failed to retrieve user.', $th->getMessage(), 500);
         }
     }
 }
