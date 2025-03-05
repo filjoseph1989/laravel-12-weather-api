43b5644aecc37270df00cc05cbc8a2f944c8e4fd
Fil <filjoseph22@gmail.com>
2025-03-05 22:57:18 +0800

    renamed allPost to getPaginatedPost, and added getPost

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\PostCollection;
 use App\Models\Post;
+use App\Http\Requests\PostStoreRequest;
+use App\Http\Resources\PostCollection;
+use App\Http\Resources\PostResource;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class PostController extends Controller
 {
@@ -14,7 +16,7 @@ class PostController extends Controller
      *
      * @return JsonResponse|mixed
      */
-    public function allPost(): JsonResponse
+    public function getPaginatedPost(): JsonResponse
     {
         try {
             $posts = Post::with('user')->paginate();
@@ -23,6 +25,41 @@ class PostController extends Controller
             return response()->json([
                 'success' => false,
                 'message' => 'An error occurred while retrieving posts.',
+                'error' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Retrieve a single post by its ID.
+     *
+     * @param int $id
+     * @return JsonResponse|mixed
+     */
+    public function getPost(Post $post): JsonResponse
+    {
+        try {
+            if (auth()->check() && auth()->id() !== $post->user_id) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'You are not authorized to view this post or post does not exist.'
+                ], 403);
+            }
+
+            $post->loadMissing('user');
+
+            $cacheKey = "post_{$post->id}";
+            $postData = Cache::remember($cacheKey, 900, fn() => new PostResource($post));
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Post retrieved successfully.',
+                'data' => $postData
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'success' => false,
+                'message' => 'An error occurred while retrieving the post.',
                 'error' => $th->getMessage()
             ], 500);
         }
