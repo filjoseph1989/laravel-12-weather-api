ae2dc5e5e7e992f2e23041bf9b634c781f461d2e
Fil <filjoseph22@gmail.com>
2025-03-06 16:39:17 +0800

    added new request for registration

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+            'name' => 'required|string|min:3'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'Email is required.',
+            'email.email' => 'Email must be a valid email address.',
+            'password.required' => 'Password is required.',
+            'password.string' => 'Password must be a string.',
+            'password.min' => 'Password must be at least 6 characters.',
+            'name.required' => 'Name is required.',
+            'name.string' => 'Name must be a string.',
+            'name.min' => 'Name must be at least 3 characters.'
+        ];
+    }
+}
